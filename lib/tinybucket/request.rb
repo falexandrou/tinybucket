90eb43c203d7cafaca30d6950e3ea45efc7053f2
Fotis Alexandrou <falexandrou@gmail.com>
2016-10-14 13:47:40 +0300

    remove the extra header

--- lib/tinybucket/request.rb
+++ lib/tinybucket/request.rb
@@ -45,7 +45,6 @@ module Tinybucket
           request.url(path, params)
         when :post, :put, :patch
           request.path = path
-          request.headers = {'Content-Type': 'application/json'} if path.include?('/deploy-keys')
           request.body = extract_data_from_params(params) unless params.empty?
         else
           raise ArgumentError, 'unknown http method: ' + method
